898bfec5bbdff0b1318e61cf39e203e0dafc8d4c
Anup Shanbhag <anupshanbhag4@gmail.com>
2025-06-01 16:47:51 -0700

    fixed app

--- app.py
+++ app.py
@@ -140,5 +140,4 @@ def clear_all_history():
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
